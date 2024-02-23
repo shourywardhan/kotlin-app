3e01d0cc40a06ca4ce95145a14bd91a6ad677041
shourywardhan <shourywardhan24@gmail.com>
2024-02-23 09:55:51 +0530

    initial commit

---
+++ src/test/kotlin/com/octobit8/kotlinapp/KotlinAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.octobit8.kotlinapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
